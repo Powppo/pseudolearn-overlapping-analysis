0249d3062092d3bdc25ecc080808207ca973c73b
ardilalukita14 <lukitasari623@gmail.com>
2023-04-19 15:21:49 +0700

    Menambah variabel baru

--- application/controllers/Soal.php
+++ application/controllers/Soal.php
@@ -74,7 +74,7 @@ class Soal extends CI_Controller
             //Jika admin maka tampilkan semua matkul
             // $data['dosen'] = $this->soal->getAllDosen();
             $data['tb_level'] = $this->level->getAlllevel();
-            $data['tipe_data'] = $this->tipe->getAllKelas();
+            $data['tipe_data'] = $this->tipe->getAllTipe();
         } else {
             //Jika bukan maka matkul dipilih otomatis sesuai matkul dosen
             // $data['dosen'] = $this->soal->getMatkulDosen($user->username);
